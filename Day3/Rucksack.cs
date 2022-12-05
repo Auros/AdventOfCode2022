f72c59489b7a9e9ebb7e999f6dc05723613e9ab4
Auros Nexus <41306347+Auros@users.noreply.github.com>
2022-12-05 08:05:14 -0500

    3.1 Whitespace Adjustment

--- Day3/Rucksack.cs
+++ Day3/Rucksack.cs
@@ -1,21 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Day3;
+﻿namespace Day3;
 
 internal class Rucksack
 {
-	public IReadOnlyList<char> FirstBag { get; }
-
-	public IReadOnlyList<char> SecondBag { get; }
-
-	public Rucksack(string content)
-	{
-		var chunks = content.Chunk(content.Length / 2);
-		FirstBag = chunks.First();
-		SecondBag = chunks.Last();
-	}
+    public IReadOnlyList<char> FirstBag { get; }
+    
+    public IReadOnlyList<char> SecondBag { get; }
+    
+    public Rucksack(string content)
+    {
+        var chunks = content.Chunk(content.Length / 2);
+        FirstBag = chunks.First();
+        SecondBag = chunks.Last();
+    }
 }
