e2545c292b798b43deac70bb1d82f21c75d42164
Auros Nexus <41306347+Auros@users.noreply.github.com>
2022-12-05 07:59:09 -0500

    3.2

---
+++ Day3/ElfGroup.cs
@@ -0,0 +1,16 @@
+﻿namespace Day3;
+
+internal class ElfGroup
+{
+    public IReadOnlyList<char> Elf1 { get; }
+    public IReadOnlyList<char> Elf2 { get; }
+    public IReadOnlyList<char> Elf3 { get; }
+
+    public ElfGroup(IEnumerable<string> elfContent)
+    {
+        var elves = elfContent.ToArray();
+        Elf1 = elves[0].ToList();
+        Elf2 = elves[1].ToList();
+        Elf3 = elves[2].ToList();
+    }
+}
--- Day3/Program.cs
+++ Day3/Program.cs
@@ -3,19 +3,29 @@
 DirectoryInfo inputDir = new(Environment.GetEnvironmentVariable("AOC_INPUT_DIR")!);
 DirectoryInfo outputDir = new(Environment.GetEnvironmentVariable("AOC_OUTPUT_DIR")!);
 
-var rucksacks = File.ReadAllLines(Path.Combine(inputDir.FullName, "3.txt"))
-    .Select(content => new Rucksack(content));
-
+var input = File.ReadAllLines(Path.Combine(inputDir.FullName, "3.txt"));
 var priorityLookup = Enumerable.Range(97, 26).Union(Enumerable.Range(65, 26)).ToList();
 
 int GetPriority(char item) => item is char.MinValue ? 0 : (priorityLookup.IndexOf(item) + 1);
 
 // - [ 3.1 ] -
 
-var sum = rucksacks.Sum(r => GetPriority(r.FirstBag.Intersect(r.SecondBag).FirstOrDefault()));
+var rucksacks = input.Select(content => new Rucksack(content));
+var rucksackUniqueSum = rucksacks.Sum(r => GetPriority(r.FirstBag.Intersect(r.SecondBag).FirstOrDefault()));
 
 // Export the output
 File.WriteAllText(Path.Combine(outputDir.FullName, "3.1.txt"), $"""
 Sum of the item priorities:
-{sum}
+{rucksackUniqueSum}
+""");
+
+// - [ 3.2 ] -
+
+var elfGroups = input.Chunk(3).Select(contents => new ElfGroup(contents));
+var elfGroupUniqueSum = elfGroups.Sum(g => GetPriority(g.Elf1.Intersect(g.Elf2).Intersect(g.Elf3).First()));
+
+// Export the output
+File.WriteAllText(Path.Combine(outputDir.FullName, "3.2.txt"), $"""
+Sum of the elf group priorities:
+{elfGroupUniqueSum}
 """);
