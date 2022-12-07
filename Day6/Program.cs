6cc117ee5aef936f5341f98de5be71468a82e4f8
Auros Nexus <41306347+Auros@users.noreply.github.com>
2022-12-07 08:14:44 -0500

    6.2

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -3,25 +3,42 @@ DirectoryInfo outputDir = new(Environment.GetEnvironmentVariable("AOC_OUTPUT_DIR
 
 var input = File.ReadAllText(Path.Combine(inputDir.FullName, "6.txt"));
 
-char[] buffer = input.Take(4).ToArray();
+
+int? GetMarker(char[] buffer)
+{
+    int? index = null;
+    for (int i = 0; i < input.Length - buffer.Length; i++)
+    {
+        if (buffer.Distinct().Count() == buffer.Length)
+        {
+            index = i + buffer.Length;
+            break;
+        }
+
+        for (int c = 0; c < buffer.Length; c++)
+            buffer[c] = input[i + c + 1];
+    }
+    return index;
+}
 
 // - [ 6.1 ] -
 
-int? index = null;
-for (int i = 0; i < input.Length - buffer.Length; i++)
-{
-    if (buffer.Distinct().Count() == buffer.Length)
-    {
-        index = i + buffer.Length;
-        break;
-    }
-
-    for (int c = 0; c < buffer.Length; c++)
-        buffer[c] = input[i + c + 1];
-}
+char[] buffer = input.Take(4).ToArray();
+int? index = GetMarker(buffer);
 
 // Export the output
 File.WriteAllText(Path.Combine(outputDir.FullName, "6.1.txt"), $"""
-First Marker Location:
+First Packet Marker Location:
+{index}
+""");
+
+// - [ 6.2 ] -
+
+buffer = input.Take(14).ToArray();
+index = GetMarker(buffer);
+
+// Export the output
+File.WriteAllText(Path.Combine(outputDir.FullName, "6.2.txt"), $"""
+First Message Marker Location:
 {index}
 """);
