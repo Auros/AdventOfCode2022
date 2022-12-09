a20478b36ad905b4b1a0f0e13fd17592e3902ec7
Auros Nexus <41306347+Auros@users.noreply.github.com>
2022-12-09 08:00:53 -0500

    9.1

---
+++ Day9/Program.cs
@@ -0,0 +1,49 @@
+﻿DirectoryInfo inputDir = new(Environment.GetEnvironmentVariable("AOC_INPUT_DIR")!);
+DirectoryInfo outputDir = new(Environment.GetEnvironmentVariable("AOC_OUTPUT_DIR")!);
+
+var input = File.ReadAllLines(Path.Combine(inputDir.FullName, "9.txt"));
+
+// - [ 9.1 ] -
+
+HashSet<(int, int)> visited = new();
+(int, int) currentHeadLocation = (0, 0);
+(int, int) currentTailLocation = (0, 0);
+foreach (var line in input)
+{
+    var (stepX, stepY) = line[0] switch
+    {
+        'U' => (0, 1),
+        'D' => (0, -1),
+        'L' => (-1, 0),
+        'R' => (1, 0),
+        _ => throw new NotImplementedException(),
+    };
+
+    var steps = int.Parse(line[2..]);
+
+    visited.Add(currentTailLocation);
+    for (int i = 0; i < steps; i++)
+    {
+        var (headX, headY) = currentHeadLocation;
+        var (tailX, tailY) = currentTailLocation;
+        var previousHeadLocation = currentHeadLocation;
+        currentHeadLocation = (headX + stepX, headY + stepY);
+        (headX, headY) = currentHeadLocation;
+
+        var diffX = headX - tailX;
+        var diffY = headY - tailY;
+
+        // If the tail is out of range of the head, move it.
+        if (diffX > 1 || diffX < -1 || diffY > 1 || diffY < -1)
+        {
+            currentTailLocation = previousHeadLocation;
+            visited.Add(currentTailLocation);
+        }
+    }
+}
+
+// Export the output
+File.WriteAllText(Path.Combine(outputDir.FullName, "9.1.txt"), $"""
+Unique Places Visited:
+{visited.Count}
+""");
